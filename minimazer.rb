d781ec6e7f1339df7e540baddab95a32ec4bf0c9
Tomislav Maljic <tomislav.maljic@gmail.com>
2016-01-13 22:08:27 +0100

    fixes

--- minimazer.rb
+++ minimazer.rb
@@ -1,7 +1,3 @@
-#initialization
-input = ARGV[0]
-w = ARGV[1].to_i
-k = ARGV[2].to_i
 
 def remove_duplicates(array)
   dup_array = []
@@ -13,23 +9,23 @@ def remove_duplicates(array)
   return dup_array
 end
 
-def left_end(str)
+def left_end(str, k, l)
   array = []
   (k..l).each do |i|
-    array.push(min_substring(str[0..i]))
+    array.push(min_substring(str[0..i], k))
   end
   return array
 end
 
-def right_end(str)
+def right_end(str, k, l)
   array = []
   (0..(l-k)).each do |i|
-    array.push(min_substring(str[i..l]))
+    array.push(min_substring(str[i..l], k))
   end
   return array
 end
 
-def min_substring (str)
+def min_substring (str, k)
   array = []
   (0..(str.length-k+1)).each do |i|
     array.push(str[i..(i+k)])
@@ -38,27 +34,30 @@ def min_substring (str)
   return array[0]
 end
 
-
 #main
+#initialization
+input_name = ARGV[0]
+w = ARGV[1].to_i
+k = ARGV[2].to_i
+l = w + k - 1
 start = Time.now
-inputFile = File.open(input, "r")
+inputFile = File.open(input_name, "r")
 input = inputFile.read.gsub("\n","").gsub("\r","")
 inputFile.close
 
-l = w + k - 1
 minimazers = []
 
 
 
-for str in left_end(inputFile[0..l])
+for str in left_end(input[0..l], k, l)
   minimazers.push(str)
 end
 
-(0..(input.length-l+1).each do |m|
-  minimazers.push(min_substring(input[m..(m+l)]))
+(0..(input.length-l+1)).each do |m|
+  minimazers.push(min_substring(input[m..(m+l)], k))
 end
 
-for str in right_end(inputFile[len(inputFile)-l+1]:)
+for str in right_end(input[(input.length-l+1)..(input.length+1)], k, l)
   minimazers.push(str)
 end
 
@@ -66,13 +65,13 @@ minimazers = remove_duplicates(minimazers)
 
 outputFile = File.open("output.txt", "w")
   minimazers.each do |m|
-	outputFile.puts m
-end
+	   outputFile.puts m
+  end
 outputFile.close
 
 finish = Time.now
 
-total_time = finish -
+total_time = finish - start
 puts "Total time elapsed: " + total_time.to_s + " seconds"
 
 total_memory = `ps -o rss -p #{$$}`.strip.split.last.to_i
