5394172f24631a3377861a59ad868e7cf376e9ec
Tomislav Maljic <tomislav.maljic@gmail.com>
2016-01-14 20:02:12 +0100

    comments

--- minimazer.rb
+++ minimazer.rb
@@ -1,3 +1,4 @@
+#remove duplicates from array
 def remove_duplicates(array)
   dup_array = []
   for str in array
@@ -8,6 +9,7 @@ def remove_duplicates(array)
   return dup_array
 end
 
+#find minimizer on left end of genome
 def left_end(str, k, l)
   array = []
   (k..l-1).each do |i|
@@ -16,6 +18,7 @@ def left_end(str, k, l)
   return array
 end
 
+#find minimizer on right end of genome
 def right_end(str, k, l)
   array = []
   (0..(l-k-1)).each do |i|
@@ -24,6 +27,7 @@ def right_end(str, k, l)
   return array
 end
 
+#find minimal substring inside of window
 def min_substring (str, k)
   array = []
   (0..(str.length-k)).each do |i|
@@ -34,6 +38,7 @@ def min_substring (str, k)
 end
 
 #main
+#console line arguments and input read
 file_name = ARGV[0]
 w = ARGV[1].to_i
 k = ARGV[2].to_i
@@ -45,24 +50,24 @@ inputFile.close
 
 minimizers = []
 
-
+#find minimizer on left end
 for str in left_end(input[0..l-1], k, l)
   minimizers.push(str)
 end
 
-puts minimizers
-
+#find minimizer
 (0..(input.length-l)).each do |m|
   minimizers.push(min_substring(input[m..(m+l-1)], k))
 end
 
-for str in right_end(input[(input.length-l)..(input.length)], k, l)
+#find minimizer on right end
+for str in right_end(input[(input.length-l+1)..(input.length)], k, l)
   minimizers.push(str)
-  puts str
 end
 
 minimizers = remove_duplicates(minimizers)
 
+#writing to file
 outputFile = File.open("output.txt", "w")
   minimizers.each do |m|
 	   outputFile.puts m
@@ -71,8 +76,10 @@ outputFile.close
 
 finish = Time.now
 
+#total elapsed time
 total_time = finish - start
 puts "Total time elapsed: " + total_time.to_s + " seconds"
 
+#total memory used
 total_memory = `ps -o rss -p #{$$}`.strip.split.last.to_i
 puts "Total memory usage: " + total_memory.to_s + "KB."
