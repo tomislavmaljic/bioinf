2d2f8d3f01288de52aa076d2759ad03354bb5802
Tomislav Maljic <tomislav.maljic@gmail.com>
2016-01-14 19:30:45 +0100

    index fixes

--- minimazer.rb
+++ minimazer.rb
@@ -10,24 +10,24 @@ end
 
 def left_end(str, k, l)
   array = []
-  (k..l).each do |i|
-    array.push(min_substring(str[0..i], k))
+  (k..l-1).each do |i|
+    array.push(min_substring(str[0..i-1], k))
   end
   return array
 end
 
 def right_end(str, k, l)
   array = []
-  (0..(l-k)).each do |i|
-    array.push(min_substring(str[i..l], k))
+  (0..(l-k-1)).each do |i|
+    array.push(min_substring(str[i..l-1], k))
   end
   return array
 end
 
 def min_substring (str, k)
   array = []
-  (0..(str.length-k+1)).each do |i|
-    array.push(str[i..(i+k)])
+  (0..(str.length-k)).each do |i|
+    array.push(str[i..(i+k-1)])
   end
   array.sort
   return array[0]
@@ -36,7 +36,7 @@ end
 #main
 file_name = ARGV[0]
 w = ARGV[1].to_i
-k = ARGV[2].to_i 
+k = ARGV[2].to_i
 l = w + k - 1
 start = Time.now
 inputFile = File.open(file_name, "r")
@@ -46,17 +46,17 @@ inputFile.close
 minimizers = []
 
 
-for str in left_end(input[0..l], k, l)
+for str in left_end(input[0..l-1], k, l)
   minimizers.push(str)
 end
 
 puts minimizers
 
-(0..(input.length-l+1)).each do |m|
-  minimizers.push(min_substring(input[m..(m+l)], k))
+(0..(input.length-l)).each do |m|
+  minimizers.push(min_substring(input[m..(m+l-1)], k))
 end
 
-for str in right_end(input[(input.length-l+1)..(input.length+1)], k, l)
+for str in right_end(input[(input.length-l)..(input.length)], k, l)
   minimizers.push(str)
   puts str
 end
